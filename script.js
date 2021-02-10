a5894ae9828a17f568d9dd4a1c84761f28616461
Lily Zheng <57676919+LZheng47@users.noreply.github.com>
2021-02-10 01:56:43 -0500

    Building WD_AS Skeleton

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 window.addEventListener("scroll", naveffect);
 
-function init(){
-    let mp = document.getElementById("mp");
-    let build = "";
-
-    for(let x = 1; x <= 4; x++){ 
-        build += `<a href="mp${x}.html">`
-        build += `  <h3> Marking Period ${x} </h3>`
-        build += `</a>`
-    }
-    
-    mp.innerHTML = build;
-}
-
 function naveffect(){
     let ypos = window.scrollY * 0.3;
     let nav = document.getElementById("nav_");
@@ -25,18 +12,6 @@ function naveffect(){
     else{
         upbutton.style.transform = "translate(0px)";
     }
-}
-
-window.addEventListener("mouseout", mainhroff);
-
-function mainhron(){
-    let mainhr = document.getElementById("mainhr");
-    mainhr.style.maxWidth = "510px";
-}
-
-function mainhroff(){
-    let mainhr = document.getElementById("mainhr");
-    mainhr.style.maxWidth = "470px";
 }
 
 function upbuttonon(){
